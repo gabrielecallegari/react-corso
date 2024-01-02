6ac3cd8d5bedf0b0a6c74aa2986dacc871e36c3c
Gabriele Callegari <g.callegari@itsrizzoli.it>
2024-01-02 21:11:29 +0100

    Add width

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="w-full bg-slate-700 h-screen justify-center items-center grid">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className='text-4xl text-white text-center' >Benvenuto nel corso React 101</h1>
+      <img src={logo} className="App-logo" alt="logo" />
+      <div className="text-center grid gap-6 justify-center " >
+        <h1 className="text-4xl text-white">
+          Benvenuto nel corso React 101
+        </h1>
+        <a className="text-blue-300 text-2xl border-b-2 border-blue-300 cursor-pointer w-fit" href="https://github.com/gabrielecallegari/react-corso">
+          Link Repo GitHub
+        </a>
+      </div>
     </div>
   );
 }
