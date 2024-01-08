8143130e348f9b807e2bbb1afb1cf9f93060e6d2
Gabriele Callegari <g.callegari@itsrizzoli.it>
2024-01-08 22:24:08 +0100

    Aggiunta funzione

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  function prova(variabile: boolean) {
+    if (variabile === true) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+
+  prova(true);
 
   return (
     <div className="w-full bg-slate-700 h-screen justify-center items-center grid">
       <img src={logo} className="App-logo" alt="logo" />
-      <div className="text-center grid gap-6 justify-center " >
-        <h1 className="text-4xl text-white">
-          Benvenuto nel corso React 101!
-        </h1>
-        <a className="text-blue-300 text-2xl border-b-2 border-blue-300 cursor-pointer w-fit" href="https://github.com/gabrielecallegari/react-corso">
+      <div className="text-center grid gap-6 justify-center ">
+        <h1 className="text-4xl text-white">Benvenuto nel corso React 101!</h1>
+        <a
+          className="text-blue-300 text-2xl border-b-2 border-blue-300 cursor-pointer w-fit"
+          href="https://github.com/gabrielecallegari/react-corso"
+        >
           Link Repo GitHub
         </a>
       </div>
