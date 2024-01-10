5af716a9a9c730a007da7ce1ef78ab9aa0d07c5b
Gabriele Callegari <g.callegari@itsrizzoli.it>
2024-01-10 22:03:49 +0100

    Better style

--- src/components/PrimoComponente/PrimoComponente.tsx
+++ src/components/PrimoComponente/PrimoComponente.tsx
@@ -3,15 +3,12 @@ interface ComponenteProps {
   secondoNumero: number;
 }
 
-const PrimoComponente = ({ 
-    primoNumero, 
-    secondoNumero }: 
-    ComponenteProps) => {
+const PrimoComponente = ({ primoNumero, secondoNumero }: ComponenteProps) => {
   console.log(primoNumero);
 
   return (
     <div>
-        <p>Secondo Numero : {secondoNumero}</p>
+      <p>Secondo Numero : {secondoNumero}</p>
       <p>primo componente</p>
     </div>
   );
