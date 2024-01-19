d7f425ec5aaa49e183e878021842fc82bdc4cea6
Gabriele Callegari <g.callegari@itsrizzoli.it>
2024-01-19 22:18:45 +0100

    Spiegazione useState

--- src/components/Card/Title/Title.tsx
+++
@@ -1,9 +0,0 @@
-const Title = () => {
-  return (
-    <div className="w-full text-center">
-      <h1 className="text-2xl font-semibold" >Lista della spesa</h1>
-    </div>
-  );
-};
-
-export default Title;
