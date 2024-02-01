e82a457d27ca3c9246917983763e9fac9889add2
bios3188 <enrico.grillo31@gmail.com>
2024-02-01 22:10:06 +0100

    lavoro del 1/2/2024

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,74 @@
 import { useState } from "react";
 import "./App.css";
+import Title from "./Components/Title/Title";
+
+export interface ProvaInterfaccia {
+    nome :string
+    cognome :string
+    eta :number
+}
 
 function App() {
-  
-  const [incremento, setIncremento] = useState<number>(0)
-  
 
-  function clicked(){
-    console.log("Click");
-    setIncremento(incremento +1)
-    
-  }
+    let risultato :number = 5.6
+    let stringa :string = "ciao"
+    let bool :boolean = true
+    interface Interfaccia2 extends ProvaInterfaccia {
+        nickname: string
+        codiceFiscale: string
+        sesso: "Maschio" | "Femmina"
+    }
+
+
+    let obj :ProvaInterfaccia = {
+        nome: "vins",
+        cognome: "grass",
+        eta: 444
+    }
+
+    let oggetto: Interfaccia2[] = [{
+        nome: "Vincenzo",
+        cognome: "grasso",
+        eta: 56,
+        nickname: "blueroseswordman",
+        codiceFiscale: "ascascascasc",
+        sesso: "Maschio",
+    }]
+
+    /*
+    * realizzare una funzione che prenda due parametri, nome ed eta,
+    * faccia il console log
+    * nome stringa
+    * eta number
+    * */
+
+    function fun(n:"Luca" | "Mario" | "Giovanni", e:number):void {
+        console.log(n, e)
+    }
+    fun("Luca", 66)
+
+
+    function miaFunz(nome: string, cognome: string, sesso?: "M" | "F"):void {
+        if(sesso === undefined) {
+            console.log(nome, cognome)
+            return
+        }
+        console.log(nome, cognome, sesso)
+    }
+    miaFunz("Vins", "Grass", "M")
+    miaFunz("Lewis", "Hamilton")
+
+
+    function mappatore() :void{
+        oggetto.map((el: Interfaccia2) => {
+            console.log(el.eta)
+        })
+    }
 
   return (
     <div className="p-5  w-full" >
-      <p>Valore variabile: {incremento} </p>
-      <button onClick={clicked} >Cliccami</button>
+        <Title />
+
     </div>
   );
 }
---
+++ src/Components/Title/Title.tsx
@@ -0,0 +1,10 @@
+import {ProvaInterfaccia} from "../../App";
+const Title = () => {
+    return (
+      <div>
+          <p> Titolo </p>
+      </div>
+    );
+};
+
+export default Title;
