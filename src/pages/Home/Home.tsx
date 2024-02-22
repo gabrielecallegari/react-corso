33e531aad88547c2cdec449413ada7a3811e78cc
Gabriele Callegari <g.callegari@itsrizzoli.it>
2024-02-22 21:27:52 +0100

    Add custom hooks

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import { useProducts } from "./hooks/useProducts";
 
 const Home = () => {
+  const { products, chiamataHooks } = useProducts();
   return (
     <div>
       <p>Sei nella home</p>
-      <Link to={"/prodotto/1"} >Vai al prodotto 1</Link>
+      <button onClick={()=>chiamataHooks()} >Pemimmio</button>
+      <Link to={"/prodotto/1"}>Vai al prodotto 1</Link>
     </div>
   );
 };
