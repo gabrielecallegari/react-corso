24632d97ba958d9301919dca524363abb598d2ca
Gabriele Callegari <g.callegari@itsrizzoli.it>
2024-02-29 21:46:49 +0100

    Add use mutation

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,26 @@
 import { Link } from "react-router-dom";
 import { useGetAllProducts } from "./hooks/GET/useGetAllProducts";
 import { useState } from "react";
+import { useMutateProduct } from "./hooks/GET/useMutateProduct";
 
 const Home = () => {
-  const [prodotti, setProdotti] = useState()
-  const { data } = useGetAllProducts(setProdotti)
-  console.log("PRODOTTI:",prodotti);
+  const [prodotti, setProdotti] = useState([]);
+  const mutate = useMutateProduct()
+  const {data} = useGetAllProducts(setProdotti);
+  const prezzo = 352
+
+  console.log(`IL PREZZO DI ${prezzo} è PER IL PRODOTTO 1`)
   
-  
+  console.log("PRODOTTI:", prodotti);
+
   return (
     <div>
       <p>Sei nella home</p>
+      {prodotti?.map((el:any)=>{
+        return (
+          <button onClick={()=>mutate.mutate(el.id)} >Prendi prodotto {el.id} </button>
+        )
+      })}
       <Link to={"/prodotto/1"}>Vai al prodotto 1</Link>
     </div>
   );
---
+++ src/pages/Home/hooks/GET/useMutateProduct.tsx
@@ -0,0 +1,25 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+
+export const useMutateProduct = () => {
+  const getProducts = async (id: any) => {
+    const response = await fetch(`https://dummyjson.com/products/${id}`);
+    return response.json();
+  };
+
+  const query = useMutation({
+    mutationKey: ["getProducts"],
+    mutationFn: getProducts,
+  });
+
+  useEffect(() => {
+    if (query.isSuccess) {
+      console.log("ID",query.data);
+    }
+    if (query.isError) {
+      console.log("ERROREW");
+    }
+  }, [query.data]);
+
+  return query;
+};
